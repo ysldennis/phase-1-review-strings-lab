b7695ac6e8cd19ba546858eca04cc34d64730a05
ysldennis <nashdennys@gmail.com>
2024-07-04 20:42:16 +0300

    jsassignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Dennis Njogu";
+
+const  welcomeMessage = "Welcome to Flatbook, ${Dennis Njogu} !"
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() + ", WELCOME TO FLATBOOK!";
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
